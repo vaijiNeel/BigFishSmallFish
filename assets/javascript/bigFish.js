131b642fcf84a58da79d880d0122d8069035cb3a
Kristin Glaze <ikglaze33@gmail.com>
2017-08-05 12:23:02 -0400

    Able to click on map to add markers dynamically. Also brought out js into the external js file.

--- assets/javascript/bigFish.js
+++ assets/javascript/bigFish.js
@@ -9,3 +9,30 @@
   };
   firebase.initializeApp(config);
   var database = firebase.database();
+
+
+  var map = null;
+  function initMap() {
+    var uluru = {lat: -25.363, lng: 131.044};
+    map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 4,
+      center: uluru
+    });
+    var marker = new google.maps.Marker({
+      position: uluru,
+      map: map
+    });
+
+    google.maps.event.addListener(map, 'click', function(event) {
+      placeMarker(event.latLng);
+    });
+  }
+  function placeMarker(latLng) {
+    var latitude = latLng.lat();
+    var longitude = latLng.lng();
+    var markerLocation = {lat: latitude, lng: longitude};
+    var marker = new google.maps.Marker({
+      position: markerLocation,
+      map: map
+    });
+  }
