ebfb9c8597181585401600b7df9a4911cece168d
Kristin Glaze <ikglaze33@gmail.com>
2017-08-05 11:52:31 -0400

    Added copypaste firebase initialization code, and external link for reset.css.

---
+++ assets/javascript/bigFish.js
@@ -0,0 +1,11 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBDETBXl4ZLh_0lrEcF-3zJLEUd25Hnji0",
+    authDomain: "fish-project-ca094.firebaseapp.com",
+    databaseURL: "https://fish-project-ca094.firebaseio.com",
+    projectId: "fish-project-ca094",
+    storageBucket: "fish-project-ca094.appspot.com",
+    messagingSenderId: "542377547666"
+  };
+  firebase.initializeApp(config);
+  var database = firebase.database();
