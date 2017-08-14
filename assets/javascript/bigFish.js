2b347e8d375bf050350cd1fdfa932b5b93f380de
vaijiNeel <nvaij@yahoo.com>
2017-08-14 09:08:11 -0400

    centerMapAtMarker function update
    centerMapAtMarker function didn't have the latest code

--- assets/javascript/bigFish.js
+++ assets/javascript/bigFish.js
@@ -260,10 +260,10 @@
   }
 
   //centers the map at clicked marker. 
-  function centerMapAtMarker(marker) {
-    //if you need animation use panTo, else use setCenter    
-    // map.setCenter(marker.getPosition());
-    map.panTo(marker.getPosition());
+  function centerMapAtMarker(lati, long) {
+    var latLng = new google.maps.LatLng(lati, long); //Makes a latlng
+    map.panTo(latLng); 
+    console.log("center map playerFish lat/lng - " + lati + ", " + long);
   }
 
   /**
